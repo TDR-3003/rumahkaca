9a62d46078fba252400dd491bb291165affbc0d2
gearrobo <gearroboone@gmail.com>
2022-03-26 10:10:27 +0700

    simpan

--- simpan-log.php
+++ simpan-log.php
@@ -2,19 +2,36 @@
 include 'conn.php';
 date_default_timezone_set('Asia/Jakarta');
 $wktu = date('Y-m-d  H:i:s');
+// var_dump($_GET);
 
-// $nama = $_GET['nama'];
-// $temp = $_GET['temp'];
-// $hum = $_GET['hum'];
 
-  var_dump($_GET);
+$api_key = $_GET['api_key'];
+$value = $_GET['value'];
 
-// $sql = "UPDATE tb_sensor SET temp = '$temp', hum = '$hum', updated_at = '$wktu' WHERE nama = '".$nama."' ";
-// if(mysqli_query($conn,$sql)){
-//     //echo "Berhasil";
-// }else{
-//     echo "gagal update";
-// }
+echo $api_key;
+echo "<br>";
+echo $value;
+echo "<br>";
+
+$ceks = query("SELECT api_key FROM sensors WHERE api_key = '".$api_key."' ");
+// var_dump($ceks);
+foreach($ceks as $cek):
+if($api_key == $cek['api_key']){
+    echo "ada sensor";
+    echo "<br>";
+    $sql = "UPDATE sensors SET value = '$value', updated_at = '$wktu' WHERE api_key = '".$api_key."' ";
+    if(mysqli_query($conn,$sql)){
+        echo "Berhasil";
+    }else{
+        echo "gagal update";
+    }
+}else {
+    echo "tidak ada sensor";
+    echo "<br>";
+}
+endforeach;
+
+
 
 // $res = " INSERT INTO tb_log_sensor (nama, temp, hum, updated_at) VALUES ('$nama','$temp','$hum','$wktu') ";
 // if(mysqli_query($conn,$res)){
