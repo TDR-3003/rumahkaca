81373e84535084740abb6cd2d2b73b0c3949ae02
gearrobo <gearroboone@gmail.com>
2022-03-28 22:39:51 +0700

    h

--- sensor.php
+++ sensor.php
@@ -2,6 +2,29 @@
 
 <?php
 date_default_timezone_set('Asia/Jakarta');
+$wktu = date('Y-m-d  H:i:s');
+
+if(isset($_POST['updatesensor'])){
+ var_dump($_POST);
+  $id = $_POST['id'];
+  $name = $_POST['name'];
+  $type = $_POST['type'];
+  $api_key = $_POST['api_key'];
+
+  $sql = " UPDATE sensors SET name = '$name' ,api_key = '$api_key' ,type = '$type'  ,updated_at = '$wktu'  WHERE id = '".$id."' ";
+  
+  
+  if(mysqli_query($conn,$sql)){
+      echo '
+          <div class="alert alert-warning" role="alert">
+              Data Sensor Berhasil di Ubah!
+          </div>
+      ';
+  }else{
+      echo "ERROR, tidak berhasil". mysqli_error($conn);
+  }
+
+}
 
 if(isset($_POST['addsensor'])){
    $name = $_POST['name'];
@@ -12,7 +35,6 @@ if(isset($_POST['addsensor'])){
    }else{
    $value = 20;
    }
-   $wktu = date('Y-m-d  H:i:s');
 
     //query
     $sql = "INSERT INTO sensors (name, type, api_key, value, created_at) VALUES ('$name','$type','$api_key','$value','$wktu') ";
@@ -81,11 +103,58 @@ if(isset($_POST['delete'])){
                         <td class="text-center"><?= $sensor['created_at']; ?></td>
                         <td class="text-center">
                         <form action="" method="post">
-                            <button type="button" class="btn btn-success">Edit</button>
+                            <a href="#" class="btn btn-success" data-toggle="modal" data-target="#updatesensor<?= $sensor['id']; ?>"> Edit</a>
                             <button type="submit" class="btn btn-danger" name="delete">Delete</button>
                             <input type="hidden" name="id" value="<?= $sensor['id'];?>">
                         </form>
                     </tr>
+                    <!-- modal update -->
+                      <div class="example-modal">
+                        <div id="updatesensor<?= $sensor['id']; ?>" class="modal fade" role="dialog" style="display:none;">
+                          <div class="modal-dialog"> 
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                  <h3 class="modal-title">Update Sensor Baru</h3>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                              </div>
+                              <div class="modal-body">
+                                <form action="" method="post" role="form">
+                                  <?php
+                                  $id = $sensor['id'];
+                                  $sqls = query("SELECT * FROM sensors WHERE id = '".$id."' ");
+                                  foreach($sqls as $sql):
+                                  ?>
+                                  <input type="hidden" name="id_user" value="<?php echo $sql['id']; ?>">
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">Name <span class="text-red">*</span></label>         
+                                    <div class="col-sm-8"><input type="text" class="form-control" name="name" placeholder="Nama Sensor" value="<?= $sql['name'];?>"></div>
+                                    </div>
+                                  </div>
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">Type <span class="text-red">*</span></label>
+                                    <div class="col-sm-8"><input type="text" class="form-control" name="type" placeholder="Type" value="<?= $sql['type'];?>"></div>
+                                    </div>
+                                  </div>
+                                  <div class="form-group">
+                                    <div class="row">
+                                    <label class="col-sm-3 control-label text-right">Api Key <span class="text-red">*</span></label>
+                                    <div class="col-sm-8"><input type="text" class="form-control" name="api_key" placeholder="Api" id="api_key" value="<?= $sql['api_key'];?>">
+                                    </div>
+                                    </div>
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="submit" class="btn btn-primary btn-user btn-block" name="updatesensor">Update</button>
+                                  </div>
+                                  <?php endforeach; ?>
+                                </form>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    <!-- modal update close -->
                     <?php endforeach; ?>
                 </tbody>
             </table>
@@ -135,7 +204,6 @@ if(isset($_POST['delete'])){
     </div>
   </div>
 </div><!-- modal insert close -->
-
 
 <?php include 'footer.php'; ?>
 
